255efbff992eadc08c1901de7efd7e86b08d5fe1
David Litwin <davidglitwin@posteo.net>
2019-10-14 16:27:32 -0600

    fix directory bugs

--- vary_Ksat_flat_base.sh
+++ vary_Ksat_flat_base.sh
@@ -13,12 +13,12 @@ module load python/3.7-anaconda
 . /software/apps/anaconda/5.2/python/3.7/etc/profile.d/conda.sh
 conda activate
 conda activate landlab_dev
-mkdir ~/data/dlitwin3/DupuitLEM/$SLURM_JOBID
-mkdir ~/data/dlitwin3/DupuitLEM/$SLURM_JOBID/data
+mkdir ~/data/dlitwin3/DupuitLEMScripts/output/Ksat_test/$SLURM_JOBID
+mkdir ~/data/dlitwin3/DupuitLEMScripts/output/Ksat_test/$SLURM_JOBID/data
 cd ~/data/dlitwin3/DupuitLEMScripts
-git rev-parse HEAD > ~/data/dlitwin3/DupuitLEM/$SLURM_JOBID/script_id.txt
+git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMScripts/output/Ksat_test/$SLURM_JOBID/script_id.txt
 cd ~/data/dlitwin3/landlab
-git rev-parse HEAD > ~/data/dlitwin3/DupuitLEM/$SLURM_JOBID/gdp_id.txt
-cp ~/data/dlitwin3/DupuitLEMScripts/vary_Ksat_flat_base.py ~/data/dlitwin3/DupuitLEM/$SLURM_JOBID
-cd ~/data/dlitwin3/DupuitLEM/$SLURM_JOBID
+git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMScripts/output/Ksat_test/$SLURM_JOBID/gdp_id.txt
+cp ~/data/dlitwin3/DupuitLEMScripts/vary_Ksat_flat_base.py ~/data/dlitwin3/DupuitLEMScripts/output/Ksat_test/$SLURM_JOBID
+cd ~/data/dlitwin3/DupuitLEMScripts/output/Ksat_test/$SLURM_JOBID
 python vary_Ksat_flat_base.py
