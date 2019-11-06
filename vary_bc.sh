709926396811eeec232b9b7dd5db9d779f0fd750
David Litwin <davidglitwin@posteo.net>
2019-11-06 14:05:25 -0700

    update shell script

--- vary_bc.sh
+++ vary_bc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=DupuitLEMTest
-#SBATCH --time=12:0:0
+#SBATCH --time=24:0:0
 #SBATCH --partition=shared
 #SBATCH --nodes=1
 # number of tasks (processes) per node
@@ -13,12 +13,12 @@ module load python/3.7-anaconda
 . /software/apps/anaconda/5.2/python/3.7/etc/profile.d/conda.sh
 conda activate
 conda activate landlab_dev
-mkdir ~/data/dlitwin3/DupuitLEMResults/$SLURM_JOBID
-mkdir ~/data/dlitwin3/DupuitLEMResults/$SLURM_JOBID/data
+mkdir ~/data/dlitwin3/DupuitLEMResults/vary_bc
+mkdir ~/data/dlitwin3/DupuitLEMResults/vary_bc/data
 cd ~/data/dlitwin3/DupuitLEMScripts
-git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMResults/$SLURM_JOBID/script_id.txt
+git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMResults/vary_bc/script_id.txt
 cd ~/data/dlitwin3/landlab
-git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMResults/$SLURM_JOBID/gdp_id.txt
-cp ~/data/dlitwin3/DupuitLEMScripts/vary_bc.py ~/data/dlitwin3/DupuitLEMResults/$SLURM_JOBID
-cd ~/data/dlitwin3/DupuitLEMResults/$SLURM_JOBID
+git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMResults/vary_bc/gdp_id.txt
+cp ~/data/dlitwin3/DupuitLEMScripts/vary_bc.py ~/data/dlitwin3/DupuitLEMResults/vary_bc
+cd ~/data/dlitwin3/DupuitLEMResults/vary_bc
 python vary_bc.py
