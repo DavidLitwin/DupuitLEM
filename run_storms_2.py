0981852a8bcefd3cc8097676b1275b29f4a3c388
David Litwin <davidglitwin@posteo.net>
2019-11-19 15:58:58 -0700

    add storm scripts

--- run_storms_2.py
+++ run_storms_2.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 30 16:49:06 2019
+Created on 19 Nov 2019
+
+First of two tests running storms for a full range of Ksat values. This is the lower set, 0.1<Ksat<2.4 m/hr
 
 @author: dgbli
 """
 import os
 import time
 import numpy as np
+from itertools import product
+import pickle
+import pandas as pd
 
 from landlab import RasterModelGrid, FIXED_VALUE_BOUNDARY, CLOSED_BOUNDARY
 from landlab.components import (
@@ -47,26 +52,32 @@ task_id = os.environ['SLURM_ARRAY_TASK_ID']
 job_id = os.environ['SLURM_ARRAY_JOB_ID']
 
 # Set parameters
-R = 1.5/(365*24*3600)  # steady, uniform recharge rate [m/s]
+events_per_year = 365 # number of storm events per year
+R_tot = 1.5  # annual recharge [m]
+dt_event = 1*3600 # event duration [s]
+dt_interevent = (365*24*3600-events_per_year*dt_event)/events_per_year
+R_event = R_tot/events_per_year/dt_event # event recharge rate [m/s]
 Ks_all = np.linspace(0.1,2.4,24)/(3600)  # hydraulic conductivity at the surface [m/s]
+Ks_print_all = (Ks_all*3600).astype(str)
 w0 = 2E-4/(365*24*3600) #max rate of soil production [m/s]
-d_k = 1.5 # characteristic depth for hydraulic conductivity [m]
+d_i_rel = 1.0 # initial depth relative to steady state depth [-]
 d_s = 1.5 # characteristic soil production depth [m]
+d_k = d_s # characteristic depth for hydraulic conductivity [m]
+d_i = d_i_rel*(-d_s*np.log(uplift_rate/w0)) # initial permeable thickness
 uplift_rate = 1E-4/(365*24*3600) # uniform uplift [m/s]
-di = 1*(-d_s*np.log(uplift_rate/w0)) # initial depth relative to steady state depth [-]
 m = 0.5 #Exponent on Q []
 n = 1.0 #Exponent on S []
 K = 5E-8 #erosivity coefficient [m-1/2 s−1/2]
 D = 0.01/(365*24*3600) # hillslope diffusivity [m2/s]
 
-id = int(task_id)
-Ks = Ks_all[id] # hydraulic conductivity
+ID = int(task_id)
+Ks_print = Ks_print_all[ID]
+Ks = Ks_all[ID] # hydraulic conductivity
 K0 = 0.01*Ks # asymptotic hydraulic conductivity at infinite depth
 
-dt_h = 1E5 # hydrological timestep [s]
 T = 1e6*(365*24*3600) # total simulation time [s]
 MSF = 500 # morphologic scaling factor [-]
-dt_m = MSF*dt_h
+dt_m = MSF*(dt_event+dt_interevent)
 N = T//dt_m
 N = int(N)
 output_interval = 2500
@@ -89,32 +100,36 @@ grid = RasterModelGrid((100, 100), xy_spacing=10.0)
 grid.set_status_at_node_on_edges(right=CLOSED_BOUNDARY, top=CLOSED_BOUNDARY, \
                               left=FIXED_VALUE_BOUNDARY, bottom=CLOSED_BOUNDARY)
 elev = grid.add_zeros('node', 'topographic__elevation')
-elev[:] = di + 0.1*np.random.rand(len(elev))
+elev[:] = d_i + 0.1*np.random.rand(len(elev))
 
 sf = SinkFillerBarnes(grid, method='D8')
 sf.run_one_step()
 
 base = grid.add_zeros('node', 'aquifer_base__elevation')
 wt = grid.add_zeros('node', 'water_table__elevation')
-wt[:] = elev
+wt[:] = elev.copy()
 gw_flux = grid.add_zeros('node', 'groundwater__specific_discharge_node')
 Kavg = avg_hydraulic_conductivity(grid,wt-base,elev-base,K0,Ks,d_k ) # depth-averaged hydraulic conductivity
 
 # initialize model components
 gdp = GroundwaterDupuitPercolator(grid, porosity=0.2, hydraulic_conductivity=Kavg, \
-                                  recharge_rate=R,regularization_f=0.01)
+                                  recharge_rate=0.0,regularization_f=0.01)
 fa = FlowAccumulator(grid, surface='topographic__elevation', flow_director='D8',  \
                      depression_finder = 'DepressionFinderAndRouter', runoff_rate='average_surface_water__specific_discharge')
 sp = FastscapeEroder(grid,K_sp = K,m_sp = m, n_sp=n,discharge_name='surface_water__discharge')
 ld = LinearDiffuser(grid, linear_diffusivity=D)
 
 # Run model forward
-num_substeps = np.zeros(N)
+num_substeps = np.zeros((N,2))
 max_rel_change = np.zeros(N)
 perc90_rel_change = np.zeros(N)
+# times = np.zeros((N,7))
 t0 = time.time()
 for i in range(N):
     elev0 = elev.copy()
+    ############### Run event ####################
+
+    # t1 = time.time()
 
     #set hydraulic conductivity based on depth
     gdp.K = avg_hydraulic_conductivity(grid,grid.at_node['aquifer__thickness'],
@@ -123,23 +138,59 @@ for i in range(N):
                                      K0,Ks,d_k,
                                      )
     #run gw model
-    gdp.run_with_adaptive_time_step_solver(dt_h,courant_coefficient=0.2)
-    num_substeps[i] = gdp._num_substeps
+    gdp.recharge = R_event
+    gdp.run_with_adaptive_time_step_solver(dt_event,courant_coefficient=0.2)
+    num_substeps[i,0] = gdp.number_of_substeps
 
-    fa.run_one_step()
+    # t2 = time.time()
+
+    qevent = grid.at_node['surface_water__specific_discharge'].copy()
+
+    # t3 = time.time()
+
+    ################ Run interevent ####################
+
+    #set hydraulic conductivity based on depth
+    gdp.K = avg_hydraulic_conductivity(grid,grid.at_node['aquifer__thickness'],
+                                     grid.at_node['topographic__elevation']-
+                                     grid.at_node['aquifer_base__elevation'],
+                                     K0,Ks,d_k,
+                                     )
+    #run gw model
+    gdp.recharge = 0.0
+    gdp.run_with_adaptive_time_step_solver(dt_interevent,courant_coefficient=0.2)
+    num_substeps[i,1] = gdp.number_of_substeps
+
+    # t4 = time.time()
+
+    qinterevent = grid.at_node['surface_water__specific_discharge'].copy()
+
+    # t5 = time.time()
 
     grid.at_node['topographic__elevation'][grid.core_nodes] += uplift_rate*dt_m
     grid.at_node['aquifer_base__elevation'][grid.core_nodes] += uplift_rate*dt_m - w0*np.exp(-(elev[grid.core_nodes]-base[grid.core_nodes])/d_s)*dt_m
 
+
+    # t6 = time.time()
+
+    grid.at_node['surface_water__specific_discharge'] = (qevent*dt_event + qinterevent*dt_interevent)/(dt_event+dt_interevent)
+    fa.run_one_step()
     sp.run_one_step(dt_m)
+
+    # t7 = time.time()
+
     ld.run_one_step(dt_m)
 
     elev[elev<base] = base[elev<base]
 
+    # t8 = time.time()
+
+    ############# record output ##############
+
     if i % output_interval == 0:
         gw_flux[:] = gdp.calc_gw_flux_at_node()
 
-        filename = './data/' + job_id + '_' + task_id + '_grid_' + str(i) + '.nc'
+        filename = './data/vary_Ksat_' + Ks_print + '_grid_' + str(i) + '.nc'
         write_raster_netcdf(
                 filename, grid, names=output_fields, format="NETCDF4")
         print('Completed loop %d' % i)
@@ -151,26 +202,30 @@ for i in range(N):
     if perc90_rel_change[i] < 1e-6:
         break
 
-t1 = time.time()
+    # times[i,:] = [t2-t1, t3-t2, t4-t3, t5-t4, t6-t5, t7-t6, t8-t7]
 
-tot_time = t1-t0
+tfin = time.time()
+tot_time = tfin-t0
+# df_times = pd.DataFrame(times,columns=['gdp_s', 'fa_s', 'gdp_i', 'fa_i', 'up', 'sp', 'ld'])
+# filename = './data/' + job_id + '_' + task_id + '_times.p'
+# pickle.dump(df_times,open(filename,'wb'))
 
 # collect output and save
 gw_flux[:] = gdp.calc_gw_flux_at_node()
 
-filename = './data/' + job_id + '_' + task_id + '_grid_end_' + str(i) + '.nc'
+filename = './data/vary_Ksat_' + Ks_print + '_grid_' + str(i) + '.nc'
 write_raster_netcdf(filename, grid, names=output_fields, format="NETCDF4")
 
-filename = './data/' + job_id + '_' + task_id + '_time' + '.txt'
+filename = './data/vary_Ksat_' + Ks_print + '_time' + '.txt'
 timefile = open(filename,'w')
 timefile.write('Run time: ' + str(tot_time))
 timefile.close()
 
-filename = './data/' + job_id + '_' + task_id + '_substeps' + '.txt'
+filename = './data/vary_Ksat_' + Ks_print + '_substeps' + '.txt'
 np.savetxt(filename,num_substeps)
 
-filename = './data/' + job_id + '_' + task_id + '_max_rel_change' + '.txt'
+filename = './data/vary_Ksat_' + Ks_print + '_max_rel_change' + '.txt'
 np.savetxt(filename,max_rel_change)
 
-filename = './data/' + job_id + '_' + task_id + '_90perc_rel_change' + '.txt'
+filename = './data/vary_Ksat_' + Ks_print + '_90perc_rel_change' + '.txt'
 np.savetxt(filename,perc90_rel_change)
---
+++ run_storms_4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=Storms_4
+#SBATCH --time=48:0:0
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+# number of tasks (processes) per node
+#SBATCH --ntasks-per-node=21
+#SBATCH --mail-type=begin,end
+#SBATCH --mail-user=dlitwin3@jhu.edu
+#### load and unload modules you may need
+module load git
+module load python/3.7-anaconda
+. /software/apps/anaconda/5.2/python/3.7/etc/profile.d/conda.sh
+conda activate
+conda activate landlab_dev
+mkdir ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID
+mkdir ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID/data
+cd ~/data/dlitwin3/DupuitLEMScripts
+git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID/script_id.txt
+cd ~/data/dlitwin3/landlab
+git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID/gdp_id.txt
+cp ~/data/dlitwin3/DupuitLEMScripts/run_storms_4.py ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID
+cd ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID
+python run_storms_4.py
