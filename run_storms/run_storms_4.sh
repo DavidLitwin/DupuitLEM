7d0feae9dc2e50ce4e887877303fdfe4ffc2a304
David Litwin <davidglitwin@posteo.net>
2020-01-30 11:46:20 -0500

    update directories

--- run_storms/run_storms_4.sh
+++ run_storms/run_storms_4.sh
@@ -13,12 +13,12 @@ module load python/3.7-anaconda
 . /software/apps/anaconda/5.2/python/3.7/etc/profile.d/conda.sh
 conda activate
 conda activate landlab_dev
-mkdir ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID
-mkdir ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID/data
+mkdir ~/data/dlitwin3/DupuitLEMResults/storms_4_A-$SLURM_ARRAY_TASK_ID
+mkdir ~/data/dlitwin3/DupuitLEMResults/storms_4_A-$SLURM_ARRAY_TASK_ID/data
 cd ~/data/dlitwin3/DupuitLEMScripts
-git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID/script_id.txt
+git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMResults/storms_4_A-$SLURM_ARRAY_TASK_ID/script_id.txt
 cd ~/data/dlitwin3/landlab
-git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID/gdp_id.txt
-cp ~/data/dlitwin3/DupuitLEMScripts/run_storms_4.py ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID
-cd ~/data/dlitwin3/DupuitLEMResults/storms_4-$SLURM_ARRAY_TASK_ID
+git rev-parse HEAD > ~/data/dlitwin3/DupuitLEMResults/storms_4_A-$SLURM_ARRAY_TASK_ID/gdp_id.txt
+cp ~/data/dlitwin3/DupuitLEMScripts/run_storms_4.py ~/data/dlitwin3/DupuitLEMResults/storms_4_A-$SLURM_ARRAY_TASK_ID
+cd ~/data/dlitwin3/DupuitLEMResults/storms_4_A-$SLURM_ARRAY_TASK_ID
 python run_storms_4.py
