6e567c1661716b1ee675f41e03e61e88413c09a0
David Litwin <davidglitwin@posteo.net>
2020-04-01 11:59:38 -0400

    calc erosion and update shear stress func

--- DupuitLEM/grid_functions/grid_funcs.py
+++ DupuitLEM/grid_functions/grid_funcs.py
@@ -42,7 +42,7 @@ def calc_shear_stress_at_node(grid, n_manning=0.05, rho = 1000, g = 9.81):
 
     where :math:`\rho` is the density of water, :math:`g` is the gravitational constant,
     :math:`S` is the topographic slope, :math:`d` is the water depth calculated with Manning's equation,
-    :math:`n` is Manning's n, :math:`q` is surface water discharge, and :math:`dx` is the grid cell
+    :math:`n` is Manning's n, :math:`Q` is surface water discharge, and :math:`dx` is the grid cell
     width.
 
     Parameters
@@ -51,10 +51,44 @@ def calc_shear_stress_at_node(grid, n_manning=0.05, rho = 1000, g = 9.81):
         Manning's n at nodes, giving surface roughness.
     rho: density of water (kg/m3)
     g: gravitational acceleration constant (m/s2)
+    Q: surface water discharge (m3/s)
+
+    notes:
+    In future, should allow n_manning and dx to be fields that are spatially variable
+
     """
     S = abs(grid.calc_grad_at_link(grid.at_node["topographic__elevation"]))
-    S_node = map_max_of_node_links_to_node(grid, S)
-    return (rho * g * S_node * (
-            (n_manning * grid.at_node["surface_water__discharge"] / 3600)
-            / (grid.dx * np.sqrt(S_node)) )**(3/5)
-    )
+    S_node = map_max_of_node_links_to_node(grid, S)[grid.core_nodes]
+    Q = grid.at_node["surface_water__discharge"][grid.core_nodes]
+
+    tau = np.zeros(grid.number_of_nodes)
+    tau[grid.core_nodes] = (rho * g * S_node * ( (n_manning * Q)/(grid.dx * np.sqrt(S_node)) )**(3/5)
+    return tau
+
+
+def calc_erosion_from_shear_stress(grid,tauc,K,b):
+    r"""
+    Calculate erosion rate dzdt (m) using the shear stress method:
+
+    .. math::
+        \frac{dz}{dt} = k (\tau - \tau_c)^b
+
+    parameters
+    ----------
+    tau: shear stress array at node (N/m2)
+    tauc: critical shear stress value (N/m2)
+    k: shear stress erosion coefficient
+    b: shear stress erosion exponent
+
+    notes:
+    Positive when eroding. Does not allow accumulation.
+    In future allow for K, tauc and b to be spatially variable
+
+    """
+    tau = grid.at_node['surface_water__shear_stress']
+
+    dzdt = np.zeros_like(tau)
+    dzdt[grid.core_nodes] = K*np.power((tau[grid.core_nodes]-tauc),b)
+    dzdt[dzdt<0.0] = 0.0
+
+    return dzdt
