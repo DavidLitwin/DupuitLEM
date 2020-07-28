0e17313f3de8af84f3141cc6cde4061554ebc375
David Litwin <davidglitwin@posteo.net>
2020-07-28 16:04:18 -0600

    fix file paths

--- scripts/HPC/run_batch_blanca.sh
+++ scripts/HPC/run_batch_blanca.sh
@@ -11,13 +11,13 @@
 #### load and unload modules you may need
 script=$1
 output_folder=$2
-mkdir projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID
-mkdir projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID/data
+mkdir /projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID
+mkdir /projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID/data
 cd ~/DupuitLEM
-git rev-parse HEAD > projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID/script_id.txt
+git rev-parse HEAD > /projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID/script_id.txt
 cd ~/landlab
-git rev-parse HEAD > projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID/gdp_id.txt
-cp ~/DupuitLEM/scripts/HPC/$script projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID
-cd projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID
+git rev-parse HEAD > /projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID/gdp_id.txt
+cp ~/DupuitLEM/scripts/HPC/$script /projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID
+cd /projects/dali4360/data/DupuitLEMResults/$output_folder-$SLURM_ARRAY_TASK_ID
 echo $SLURM_JOBID-$SLURM_ARRAY_TASK_ID > slurm.txt
 python -u $script > pythonlog.out
