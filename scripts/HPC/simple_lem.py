f18441f9e8065d95bf1e7df8e55b115a7a9e9e16
David Litwin <davidglitwin@posteo.net>
2020-08-27 15:07:24 -0400

    simple_lem_1_9

--- scripts/HPC/simple_lem.py
+++ scripts/HPC/simple_lem.py
@@ -9,6 +9,7 @@ nondimensionalization introduced by Nikos Theodoratos.
 """
 
 import os
+import time
 import numpy as np
 
 from landlab import RasterModelGrid
@@ -24,22 +25,28 @@ task_id = os.environ['SLURM_ARRAY_TASK_ID']
 ID = int(task_id)
 base_path = './data/simple_lem_1_'
 
-lg_all = np.array([10, 20, 40, 80])
+times = np.zeros(2)
+t0 = time.time()
+
+dx = 16.0
+lg_all = dx/np.array([0.9, 0.8, 0.75, 0.7, 0.6])
 
 lg = lg_all[ID]
 
-D = 0.002 #m2/yr
+D = 0.01 #m2/yr
 K = D/lg**2
 U = 1e-4 #m/yr
 m = 0.5
 n = 1
 
-T = 500*(1/K)
-dt = 5e-6*T
+hg = U/K
+T = 600*(1/K)
+dt = 5e-3*(1/K)
 N = int(T//dt)
+output_interval = 1000
 
-np.random.seed(1234)
-grid = RasterModelGrid((100,100), xy_spacing=lg/2)
+np.random.seed(12345)
+grid = RasterModelGrid((125,125), xy_spacing=dx)
 # grid.set_status_at_node_on_edges(right=grid.BC_NODE_IS_CLOSED,
 #                                 top=grid.BC_NODE_IS_CLOSED,
 #                                 left=grid.BC_NODE_IS_FIXED_VALUE,
@@ -51,13 +58,15 @@ grid.set_status_at_node_on_edges(right=grid.BC_NODE_IS_FIXED_VALUE,
                                 bottom=grid.BC_NODE_IS_FIXED_VALUE,
 )
 z = grid.add_zeros('node', 'topographic__elevation')
-z[:] = 0.01*(U/K)*np.random.rand(len(z))
+z[:] = 0.01*hg*np.random.rand(len(z))
 
 
 fa = FlowAccumulator(grid, surface='topographic__elevation', flow_director='D8', depression_finder='LakeMapperBarnes')
 ld = LinearDiffuser(grid, D)
 sp = FastscapeEroder(grid, K_sp=K, m_sp=m, n_sp=n)
 
+t1 = time.time()
+times[0] = t1 - t0
 for i in range(N):
 
     z[grid.core_nodes] += U*dt
@@ -67,7 +76,9 @@ for i in range(N):
     sp.run_one_step(dt)
 
 
-    if i%4000==0:
+    if i%output_interval==0:
         print('finished iteration %d'%i)
         filename = base_path + '%d_grid_%d.nc'%(ID,i)
         write_raster_netcdf(filename, grid, names = "topographic__elevation", format="NETCDF4")
+times[1] = time.time() - t1
+np.savetxt('times.out', times, delimeter=',')
