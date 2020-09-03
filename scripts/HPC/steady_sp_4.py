b12c71d54f5e98c06fcdeb46643d352144ba176b
David Litwin <davidglitwin@posteo.net>
2020-09-03 15:20:26 -0400

    steady with hex grid steady_sp_4_1

---
+++ scripts/HPC/steady_sp_4.py
@@ -0,0 +1,185 @@
+"""
+Steady recharge + constant thickness + StreamPowerModel
+
+This script uses dimensionless parameters based on Theodoratos method of
+nondimensionalizing the governing landscape evolution equation. Vary lambda
+and gamma.
+
+Same as steady_sp_3 but using a HexModelGrid
+
+\[lambda] == (p K D)/(ks U^2),
+\[Gamma] == (ks b U)/(p D),
+
+Date: 1 Sept 2020
+"""
+import os
+import numpy as np
+from itertools import product
+import pandas
+import pickle
+
+from landlab import HexModelGrid
+from landlab.components import (
+    GroundwaterDupuitPercolator,
+    LinearDiffuser,
+    FastscapeEroder,
+    FlowDirectorSteepest,
+    FlowAccumulator,
+    LakeMapperBarnes,
+    )
+from DupuitLEM import StreamPowerModel
+from DupuitLEM.auxiliary_models import (
+    HydrologySteadyStreamPower,
+    RegolithConstantThickness,
+    )
+
+#slurm info
+task_id = os.environ['SLURM_ARRAY_TASK_ID']
+ID = int(task_id)
+
+#dim equations
+def b_fun(U, K, gam, lam):
+    return (U*gam*lam)/K
+
+def ksat_fun(D, U, p, K, lam):
+    return (D*p*K)/(U**2*lam)
+
+#generate dimensioned parameters
+def generate_parameters(D, U, K, p, n, gam, lam):
+
+    b = b_fun(U, K, gam, lam)
+    ksat = ksat_fun(D, U, p, K, lam)
+
+    return K, D, U, ksat, p, b, n, gam, lam
+
+#parameters
+lam_all = np.geomspace(0.2,2,5)
+gam_all = np.geomspace(0.5,5,5)
+lg = 15
+D1 = 0.01/(365*24*3600) # hillslope linear diffusivity [m2/s]
+U1 = 1e-4/(365*24*3600) # Uplift rate [m/s]
+K1 = (D1/lg**2) # Streampower incision coefficient [1/s]
+n1 = 0.1 # drainable porosity [-]
+p1 = 0.75/(365*24*3600) # steady precipitation rate
+
+Tg_nd = 1500 # total duration in units of tg [-]
+dtg_nd = 5e-3 # geomorphic timestep in units of tg [-]
+Th_nd = 5 # hydrologic time in units of t_vn [-]
+
+lam1 = np.array(list(product(lam_all, gam_all)))[:,0]
+gam1 = np.array(list(product(lam_all, gam_all)))[:,1]
+
+params = np.zeros((len(lam1),9))
+for i in range(len(lam1)):
+
+    params[i,:] = generate_parameters(D1, U1, K1, p1, n1, gam1[i], lam1[i])
+
+df_params = pandas.DataFrame(params,columns=['K', 'D', 'U', 'ksat', 'p', 'b', 'n', 'gam', 'lam'])
+df_params['hg'] = df_params['U']/df_params['K'] # characteristic geomorphic vertical length scale [m]
+df_params['lg'] = np.sqrt(df_params['D']/df_params['K']) # characteristic geomorphic horizontal length scale [m]
+df_params['tg'] = 1/df_params['K'] # characteristic geomorphic timescale [s]
+df_params['tfill'] = (df_params['n']*df_params['b'])/df_params['p']
+df_params['tdrain'] = (df_params['D']*df_params['n'])/(df_params['ksat']*df_params['U'])
+df_params['Tg'] = Tg_nd*df_params['tg'] # Total geomorphic simulation time [s]
+df_params['dtg'] = dtg_nd*df_params['tg'] # geomorphic timestep [s]
+df_params['Th'] = Th_nd*(df_params['n']*0.8*df_params['lg'])/(4*df_params['ksat']*df_params['b']) # hydrologic simulation time in units of von neumann stability time [s]
+df_params['MSF'] = df_params['dtg']/df_params['Th'] # morphologic scaling factor
+
+pickle.dump(df_params, open('parameters.p','wb'))
+
+ksat = df_params['ksat'][ID]
+p = df_params['p'][ID]
+b = df_params['b'][ID]
+n = df_params['n'][ID]
+
+K = df_params['K'][ID]
+Ksp = K/p #see governing equation. If the discharge field is (Q/sqrt(A)) then streampower coeff is K/p
+D = df_params['D'][ID]
+U = df_params['U'][ID]
+hg = df_params['hg'][ID]
+lg = df_params['lg'][ID]
+
+Th = df_params['Th'][ID]
+Tg = df_params['Tg'][ID]
+MSF = df_params['MSF'][ID]
+Nx = 100
+
+output = {}
+output["output_interval"] = 1000
+output["output_fields"] = [
+        "at_node:topographic__elevation",
+        "at_node:aquifer_base__elevation",
+        "at_node:water_table__elevation",
+        ]
+output["base_output_path"] = './data/steady_sp_4_'
+output["run_id"] = ID #make this task_id if multiple runs
+
+#initialize grid
+np.random.seed(12345)
+grid = HexModelGrid((Nx, Nx), node_layout="rect", spacing=0.7*lg)
+indices = np.arange(Nx**2).reshape((Nx,Nx))
+indices_flat = np.arange(Nx**2)
+grid.status_at_node[indices_flat%Nx==0] = 4
+grid.status_at_node[(indices_flat-Nx-1)%Nx==0] = 4
+grid.status_at_node[indices_flat>=Nx*(Nx-1)] = 4
+
+elev = grid.add_zeros('node', 'topographic__elevation')
+elev[:] = b + 0.1*hg*np.random.rand(len(elev))
+base = grid.add_zeros('node', 'aquifer_base__elevation')
+wt = grid.add_zeros('node', 'water_table__elevation')
+wt[:] = elev.copy()
+
+#initialize landlab components
+gdp = GroundwaterDupuitPercolator(grid,
+        porosity=n,
+        hydraulic_conductivity=ksat,
+        regularization_f=0.01,
+        recharge_rate=p,
+        courant_coefficient=0.9,
+        vn_coefficient = 0.9,
+)
+fd = FlowDirectorSteepest(grid)
+fa = FlowAccumulator(grid,
+				        surface='topographic__elevation',
+						flow_director=fd,
+						runoff_rate='average_surface_water__specific_discharge')
+lmb = LakeMapperBarnes(grid, method='Steepest', fill_flat=False,
+						  surface='topographic__elevation',
+						  fill_surface='topographic__elevation',
+						  redirect_flow_steepest_descent=False,
+						  reaccumulate_flow=False,
+						  track_lakes=False,
+						  ignore_overfill=True)
+ld = LinearDiffuser(grid, linear_diffusivity=D)
+
+#initialize other models
+hm = HydrologySteadyStreamPower(
+        grid,
+        groundwater_model=gdp,
+        flow_director=fd,
+        flow_accumulator=fa,
+        lake_mapper=lmb,
+        hydrological_timestep=Th,
+)
+
+#use surface_water_area_norm__discharge (Q/sqrt(A)) for Theodoratos definitions
+sp = FastscapeEroder(grid,
+        K_sp=Ksp,
+        m_sp=1,
+        n_sp=1,
+        discharge_field="surface_water_area_norm__discharge",
+)
+rm = RegolithConstantThickness(grid, equilibrium_depth=b, uplift_rate=U)
+
+mdl = StreamPowerModel(grid,
+        hydrology_model=hm,
+        diffusion_model=ld,
+        erosion_model=sp,
+        regolith_model=rm,
+        morphologic_scaling_factor=MSF,
+        total_morphological_time=Tg,
+        verbose=True,
+        output_dict=output,
+)
+
+mdl.run_model()
