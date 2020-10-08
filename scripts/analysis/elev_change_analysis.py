b5aa75705bae6ff283d1909d42b01317a8e206bc
David Litwin <davidglitwin@posteo.net>
2020-10-08 16:16:56 -0400

    add relief change

--- scripts/analysis/elev_change_analysis.py
+++ scripts/analysis/elev_change_analysis.py
@@ -1,5 +1,5 @@
 """
-Analysis of results on HPC for steady stream power model runs.
+Analysis of elevation change when steady state scheme script has been run.
 """
 
 import os
@@ -28,13 +28,13 @@ dt = dt_nd*df_params['tg'][ID]
 
 ####### calculate elevation change
 z_change = np.zeros((len(files),7))
-tot_relief = np.zeros(len(files))
+relief_change = np.zeros(len(files), 2)
 try:
     grid = from_netcdf(files[0])
 except KeyError:
     grid = read_netcdf(files[0])
 elev0 = grid.at_node['topographic__elevation']
-tot_relief[0] = np.sum(elev0)
+relief_change[0,0] = np.mean(elev0)
 for i in range(1,len(files)):
 
     try:
@@ -54,45 +54,46 @@ for i in range(1,len(files)):
 
     elev0 = elev.copy()
 
-    tot_relief[i] = np.sum(elev)
+    relief_change[i,0] = np.mean(elev)
+    relief_change[i,1] = np.mean(elev) - relief_change[i-1,1]
 
-    if i%100==0:
+    # if i%100==0:
+    #
+    #     field = np.log10((elev_diff/dt)/df_params['U'][ID])
+    #     y = np.arange(grid.shape[0] + 1) * grid.dy - grid.dy * 0.5
+    #     x = np.arange(grid.shape[1] + 1) * grid.dx - grid.dx * 0.5
+    #
+    #     plt.figure(figsize=(8,6))
+    #     im = plt.imshow(field.reshape(grid.shape).T,
+    #                      origin="lower",
+    #                      extent=(x[0], x[-1], y[0], y[-1]),
+    #                      cmap='plasma',
+    #                      vmin=-10,
+    #                      vmax=0.0,
+    #                      )
+    #     plt.colorbar(im, label='log10(dzdt/U)')
+    #     plt.title('ID %d, Iteration %d'%(ID,i))
+    #     plt.savefig('../post_proc/%s/elev_change_%d_%d.png'%(base_output_path, ID, i))
+    #     plt.close()
 
-        field = np.log10((elev_diff/dt)/df_params['U'][ID])
-        y = np.arange(grid.shape[0] + 1) * grid.dy - grid.dy * 0.5
-        x = np.arange(grid.shape[1] + 1) * grid.dx - grid.dx * 0.5
 
-        plt.figure(figsize=(8,6))
-        im = plt.imshow(field.reshape(grid.shape).T,
-                         origin="lower",
-                         extent=(x[0], x[-1], y[0], y[-1]),
-                         cmap='plasma',
-                         vmin=-10,
-                         vmax=0.0,
-                         )
-        plt.colorbar(im, label='log10(dzdt/U)')
-        plt.title('ID %d, Iteration %d'%(ID,i))
-        plt.savefig('../post_proc/%s/elev_change_%d_%d.png'%(base_output_path, ID, i))
-        plt.close()
-
-
-        cmap = plt.get_cmap('plasma')
-        fig = plt.figure()
-        sm = ScalarMappable(cmap=cmap, norm=Normalize(vmin=-10, vmax=1.0))
-        rgb = sm.to_rgba(field.reshape(grid.shape).T, alpha=0.7)
-        ls = LightSource(azdeg=135, altdeg=45)
-        sr = ls.shade_rgb(rgb, elev.reshape(grid.shape).T, blend_mode='hsv', vert_exag=1.2, dx=grid.dx, dy=grid.dy)
-        plt.imshow(sr, origin="lower", extent=(x[0], x[-1], y[0], y[-1]),)
-        cbar = fig.colorbar(sm, label='log10(dzdt/U)')
-        # cbar.solids.set_edgecolor("face")
-        plt.title('ID %d, Iteration %d'%(ID,i))
-        plt.savefig('../post_proc/%s/elev_change_hs_%d_%d.png'%(base_output_path, ID, i))
-        plt.close()
+        # cmap = plt.get_cmap('plasma')
+        # fig = plt.figure()
+        # sm = ScalarMappable(cmap=cmap, norm=Normalize(vmin=-10, vmax=1.0))
+        # rgb = sm.to_rgba(field.reshape(grid.shape).T, alpha=0.7)
+        # ls = LightSource(azdeg=135, altdeg=45)
+        # sr = ls.shade_rgb(rgb, elev.reshape(grid.shape).T, blend_mode='hsv', vert_exag=1.2, dx=grid.dx, dy=grid.dy)
+        # plt.imshow(sr, origin="lower", extent=(x[0], x[-1], y[0], y[-1]),)
+        # cbar = fig.colorbar(sm, label='log10(dzdt/U)')
+        # # cbar.solids.set_edgecolor("face")
+        # plt.title('ID %d, Iteration %d'%(ID,i))
+        # plt.savefig('../post_proc/%s/elev_change_hs_%d_%d.png'%(base_output_path, ID, i))
+        # plt.close()
 
 df_z_change = pd.DataFrame(z_change,columns=['max', '95 perc', '90 perc', '50 perc', '10 perc', 'min', 'mean'])
-
-pickle.dump(df_z_change, open('../post_proc/%s/z_change_steady_state_%d.p'%(base_output_path, ID), 'wb'))
-
+df_z_change['mean relief'] = relief_change[i,0]
+df_z_change['mean relief change'] = relief_change[i,1]
+df_z_change.to_csv('../post_proc/%s/z_change_steady_state_%d.csv'%(base_output_path, ID))
 
 dzdt = (df_z_change/dt)
 t = np.arange(1,len(dzdt))*dt_nd
@@ -115,9 +116,9 @@ plt.savefig('../post_proc/%s/elev_change_ss_%d.png'%(base_output_path, ID), dpi=
 plt.close()
 
 plt.figure()
-plt.plot(t,tot_relief[1:])
+plt.plot(t,relief_change[1:,1])
 plt.xlabel('$t/t_g$ [yr]')
-plt.ylabel('sum(z) [m/yr]')
+plt.ylabel('mean(z) [m]')
 plt.tight_layout()
 plt.savefig('../post_proc/%s/tot_relief_%d.png'%(base_output_path, ID), dpi=300)
 plt.close()
