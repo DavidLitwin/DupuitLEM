9cd7587deb69731ef5dc6285bc49c30d28505d03
David Litwin <davidglitwin@posteo.net>
2020-11-20 13:34:12 -0500

    tests for grid cell dependence

--- scripts/HPC/simple_lem_4.py
+++ scripts/HPC/simple_lem_4.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 27 12:11:59 2020
 
 A simple streampower diffusion model, to test the grid scale dependence of
 the solution with lg, the geomorphic length scale defined by Theodoratos.
 
+Implement a correction factor for grid scale dependence
+
 @author: dgbli
+20 Nov 2020
+
 """
 
 import os
@@ -22,7 +25,7 @@ from landlab.io.netcdf import to_netcdf
 
 task_id = os.environ['SLURM_ARRAY_TASK_ID']
 ID = int(task_id)
-base_path = './data/simple_lem_2_'
+base_path = './data/simple_lem_4_'
 
 lg = 15
 Lx_nd = 87.5
@@ -32,8 +35,11 @@ Nx_all = (Lx_nd*lg+1e-10)//dx_all
 dx = dx_all[ID]
 Nx = int(Nx_all[ID])
 
+w0 = lg
+A0 = lg**2
+
 D = 0.01 #m2/yr
-K = D/lg**2
+K = (D/lg**2)*(dx/w0)
 U = 1e-4 #m/yr
 m = 0.5
 n = 1
@@ -43,8 +49,6 @@ T = 800*(1/K)
 dt = 5e-3*(1/K)
 N = int(T//dt)
 output_interval = 1000
-# calc_rate_of_change = lambda elev, elev0, dtm, N: np.mean(abs(elev-elev0))/(N*dtm)
-# stop_rate = 1e-4
 
 np.random.seed(12345)
 grid = RasterModelGrid((Nx, Nx), xy_spacing=dx)
@@ -56,7 +60,9 @@ z[:] = 0.1*hg*np.random.rand(len(z))
 
 fa = FlowAccumulator(grid, surface='topographic__elevation', flow_director='D8', depression_finder='LakeMapperBarnes')
 ld = LinearDiffuser(grid, D)
-sp = FastscapeEroder(grid, K_sp=K, m_sp=m, n_sp=n)
+
+accum = grid.add_zeros('node', 'width_norm_area')
+sp = FastscapeEroder(grid, K_sp=K, m_sp=m, n_sp=n, discharge_field='width_norm_area')
 
 for i in range(N):
 
@@ -64,6 +70,7 @@ for i in range(N):
 
     ld.run_one_step(dt)
     fa.run_one_step()
+    accum[:] = grid.at_node['drainage_area']*(A0/dx**2)
     sp.run_one_step(dt)
 
     print('completed loop %d'%i)
@@ -73,15 +80,3 @@ for i in range(N):
         filename = base_path + '%d_grid_%d.nc'%(ID,i)
         to_netcdf(grid, filename, include="at_node:topographic__elevation")
 
-
-    # check stopping condition
-    # if i > 0:
-    #
-    #     filename0 = base_path + str(ID) + '_grid_' + str(i-output_interval) + '.nc'
-    #     grid0 = read_netcdf(filename0)
-    #     elev0 = grid0.at_node['topographic__elevation']
-    #     dzdt = calc_rate_of_change(elev, elev0, dt, output_interval)
-    #
-    #     if dzdt < stop_rate:
-    #         print('Stopping rate condition met, dzdt = %.4e'%dzdt)
-    #         break
