b6171be069399efd5748d728bc94b42b4eff8233
David Litwin <davidglitwin@posteo.net>
2020-12-03 14:12:17 -0500

    add hillshades

--- scripts/analysis/make_gifs.py
+++ scripts/analysis/make_gifs.py
@@ -6,13 +6,16 @@ Created on Wed Jul  1 15:19:58 2020
 
 @author: dgbli
 """
-import matplotlib.pyplot as plt
-from landlab import imshow_grid
-from landlab.io.netcdf import from_netcdf
 import numpy as np
 import os
 import glob
 import imageio
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LightSource
+from landlab import imshow_grid
+from landlab.io.netcdf import from_netcdf
+
 
 task_id = os.environ['SLURM_ARRAY_TASK_ID']
 ID = int(task_id)
@@ -31,8 +34,7 @@ grid = from_netcdf(path)
 elev = grid.at_node['topographic__elevation']
 
 
-def plot_elev(file,index):
-
+def plot_elev(file, index):
     grid = from_netcdf(file)
 
     fig = plt.figure(figsize=(8,6))
@@ -44,7 +46,26 @@ def plot_elev(file,index):
     image  = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
 
     plt.close()
-
     return image
 
-imageio.mimsave('../post_proc/%s/elev_ID_%d.gif'%(base_output_path, ID), [plot_elev(files[i],indices[i]) for i in range(len(files))], fps=2)
+def plot_hillshade(file, index):
+    grid = from_netcdf(file)
+    elev_plt = grid.at_node['topographic__elevation']
+    y = np.arange(grid.shape[0] + 1) * grid.dy - grid.dy * 0.5
+    x = np.arange(grid.shape[1] + 1) * grid.dx - grid.dx * 0.5
+
+    ls = LightSource(azdeg=135, altdeg=45)
+    fig = plt.figure(figsize=(8,6))
+    plt.imshow(ls.hillshade(elev_plt.reshape(grid.shape).T, vert_exag=2, dx=grid.dx, dy=grid.dy), origin="lower", extent=(x[0], x[-1], y[0], y[-1]), cmap='gray')
+    plt.tight_layout()
+
+    fig.canvas.draw()       # draw the canvas, cache the renderer
+    image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+    image  = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+    plt.close()
+    return image
+
+imageio.mimsave('../post_proc/%s/elev_ID_%d.gif'%(base_output_path, ID), [plot_elev(files[i],indices[i]) for i in range(len(files))], fps=4)
+
+imageio.mimsave('../post_proc/%s/hillshade_ID_%d.gif'%(base_output_path, ID), [plot_hillshade(files[i],indices[i]) for i in range(len(files))], fps=4)
