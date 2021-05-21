2fe667f4158162c55ba9177bef146a58af50e830
David Litwin <davidglitwin@posteo.net>
2021-05-21 15:46:15 -0400

    remove callback, write max min water table

--- scripts/misc/test_hydrology_event_vadose.py
+++ scripts/misc/test_hydrology_event_vadose.py
@@ -104,21 +104,6 @@ wt = grid.add_zeros('node', 'water_table__elevation')
 wt[grid.core_nodes] = base[grid.core_nodes] + wtrel0*b
 wt[grid.open_boundary_nodes] = elev[grid.open_boundary_nodes]
 
-f = open('./gdp_flux_state_%d.csv'%ID, 'w')
-def write_SQ(grid, r, dt, file=f):
-    cores = grid.core_nodes
-    h = grid.at_node["aquifer__thickness"]
-    area = grid.cell_area_at_node
-    storage = np.sum(n*h[cores]*area[cores])
-
-    qs = grid.at_node["surface_water__specific_discharge"]
-    qs_tot = np.sum(qs[cores]*area[cores])
-    qs_nodes = np.sum(qs[cores]>1e-10)
-
-    r_tot = np.sum(r[cores]*area[cores])
-
-    file.write('%f, %f, %f, %f, %f\n'%(dt, r_tot, qs_tot, storage, qs_nodes))
-
 # initialize landlab components
 gdp = GroundwaterDupuitPercolator(grid,
                                   porosity=n,
@@ -126,7 +111,6 @@ gdp = GroundwaterDupuitPercolator(grid,
                                   recharge_rate=0.0,
                                   vn_coefficient=0.5,
                                   courant_coefficient=0.5,
-                                  callback_fun=write_SQ,
                                   )
 pdr = PrecipitationDistribution(grid,
                                mean_storm_duration=tr,
@@ -151,7 +135,6 @@ hm = HydrologyEventVadoseStreamPower(
 
 
 hm.run_step_record_state()
-f.close()
 
 # effective Qstar
 Q_all = hm.Q_all[1:,:]
@@ -179,6 +162,8 @@ thresh = 1e-10 #np.mean(grid.cell_area_at_node[grid.core_nodes])*df_params['p'][
 sat_all = (Q_all > thresh)
 wtrel_end_interstorm = grid.add_zeros('node', 'wtrel_mean_end_interstorm')
 wtrel_end_storm = grid.add_zeros('node', 'wtrel_mean_end_storm')
+wtrel_max = grid.add_zeros('node', 'wtrel_max')
+wtrel_min = grid.add_zeros('node', 'wtrel_min')
 sat_end_interstorm = grid.add_zeros('node', 'sat_mean_end_interstorm')
 sat_end_storm = grid.add_zeros('node', 'sat_mean_end_storm')
 Q_end_interstorm = grid.add_zeros('node', 'Q_mean_end_interstorm')
@@ -186,6 +171,8 @@ Q_end_storm = grid.add_zeros('node', 'Q_mean_end_storm')
 
 wtrel_end_storm[:] = np.mean(wtrel_all[intensity>0,:], axis=0)
 wtrel_end_interstorm[:] = np.mean(wtrel_all[intensity==0.0,:], axis=0)
+wtrel_max[:] = np.max(wtrel_all, axis=0)
+wtrel_max[:] = np.min(wtrel_all, axis=0)
 sat_end_storm[:] = np.mean(sat_all[intensity>0,:], axis=0)
 sat_end_interstorm[:] = np.mean(sat_all[intensity==0.0,:], axis=0)
 Q_end_storm[:] = np.mean(Q_all[intensity>0,:], axis=0)
@@ -196,49 +183,7 @@ mean_r_storm = grid.add_zeros('node', 'mean_recharge_storm')
 mean_r_storm[:] = np.mean(hm.r_all[hm.intensity>0,:], axis=0)
 p_tot = np.sum(np.array(hm.intensities)*np.array(hm.storm_dts))
 
-#load the full storage discharge dataset that was just generated
-df = pd.read_csv('./gdp_flux_state_%d.csv'%ID, sep=',',header=None, names=['dt','r', 'qs', 'S', 'qs_cells'])
-# remove the first row (test row written by init of gdp)
-df.drop(0, inplace=True)
-df.reset_index(drop=True, inplace=True)
-# make dimensionless timeseries
-Atot = np.sum(grid.cell_area_at_node[grid.core_nodes])
-df['qs_star'] = df['qs']/(p*Atot)
-df['S_star'] = df['S']/(b*n*Atot)
-
-######## Runoff generation
-# find times with recharge. Note in df qs and S are at the end of the timestep.
-# i is at the beginning of the timestep. Assumes timeseries starts with recharge.
-df['t'] = np.cumsum(df['dt'])
-is_recharge = df['r'] > 0.0
-pre_recharge = np.where(np.diff(is_recharge*1)>0.0)[0] #last data point before recharge
-end_recharge = np.where(np.diff(is_recharge*1)<0.0)[0][1:] #last data point where there is recharge
-
-# make a linear-type baseflow separation, where baseflow during recharge increases
-# linearly from its initial qs before recharge to its final qs after recharge.
-qb = df['qs'].copy()
-q = df['qs']
-t = df['t']
-for i in range(len(pre_recharge)-1): # added -1 but FIX THIS.
-    slope = (q[end_recharge[i]+1] - q[pre_recharge[i]])/(t[end_recharge[i]+1]-t[pre_recharge[i]])
-    qb[pre_recharge[i]+1:end_recharge[i]+1] = q[pre_recharge[i]]+slope*(t[pre_recharge[i]+1:end_recharge[i]+1] - t[pre_recharge[i]])
-
-df['qb'] = qb
-qe = df['qs'] - df['qb']
-
 df_output = {}
-
-# integrate to find total values. trapezoidal for the properties that
-# change dynamically, and simple rectangular for i bc we know recharge varies in this way.
-df_output['qe_tot'] = np.trapz(qe, df['t'])
-df_output['qb_tot'] = np.trapz(qb, df['t'])
-df_output['qs_tot'] = np.trapz(df['qs'], df['t'])
-df_output['r_tot'] = np.sum(df['dt'] * df['r']) # total recharge
-df_output['p_tot'] = p_tot # this is tanek from the hm, since gdp only sees recharge
-
-df_output['BFI'] = df_output['qb_tot']/df_output['qs_tot'] #baseflow index
-df_output['RR'] = df_output['qe_tot']/df_output['p_tot'] #runoff ratio
-
 df_output['mean recharge depth profile'] = hm.mean_recharge_depth
 df_output['recharge frequency depth profile'] = hm.recharge_frequency
 
@@ -249,6 +194,8 @@ output_fields = [
         "at_node:qstar_mean_no_interevent",
         "at_node:wtrel_mean_end_storm",
         "at_node:wtrel_mean_end_interstorm",
+        "at_node:wtrel_max",
+        "at_node:wtrel_min",
         "at_node:sat_mean_end_storm",
         "at_node:sat_mean_end_interstorm",
         "at_node:Q_mean_end_storm",
@@ -257,4 +204,3 @@ output_fields = [
 to_netcdf(grid, 'grid_%d.nc'%ID, include=output_fields, format="NETCDF4")
 
 pickle.dump(df_output, open('output_%d.p'%ID, 'wb'))
-pickle.dump(df, open('gdp_flux_state_%d.p'%ID, 'wb'))
