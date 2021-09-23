99b6f0fe0b34dd55ea86fc8bd477e571942c5b50
David Litwin <davidglitwin@posteo.net>
2021-09-23 15:58:41 -0400

    new saturation and entropy analysis

--- scripts/analysis/stoch_sp_analysis.py
+++ scripts/analysis/stoch_sp_analysis.py
@@ -262,6 +262,26 @@ sat_end_interstorm[:] = np.mean(sat_all[intensity==0.0,:], axis=0)
 Q_end_storm[:] = np.mean(Q_all[intensity>0,:], axis=0)
 Q_end_interstorm[:] = np.mean(Q_all[intensity==0.0,:], axis=0)
 
+# classify zones of saturation
+sat_class = grid.add_zeros('node', 'saturation_class')
+sat_never = np.logical_and(sat_end_storm < 0.001, sat_end_interstorm < 0.001)
+sat_always = np.logical_and(sat_end_interstorm > 0.999, sat_end_storm > 0.999)
+sat_variable = ~np.logical_or(sat_never, sat_always)
+sat_class[sat_never] = 0
+sat_class[sat_variable] = 1
+sat_class[sat_always] = 2
+df_output['sat_never'] = np.sum(sat_never[grid.core_nodes])/grid.number_of_core_nodes
+df_output['sat_variable'] = np.sum(sat_variable[grid.core_nodes])/grid.number_of_core_nodes
+df_output['sat_always'] = np.sum(sat_always[grid.core_nodes])/grid.number_of_core_nodes
+
+# saturtion probability and entropy
+sat_prob = grid.add_zeros('node', 'saturation_probability')
+sat_entropy = grid.add_zeros('node', 'saturation_entropy')
+calc_entropy = lambda x: -x*np.log2(x) - (1-x)*np.log2(1-x)
+sat_prob[:] = np.sum((sat_all.T*dt)/np.sum(dt), axis=1)
+sat_entropy[:] = calc_entropy(sat_prob)
+df_output['total_sat_entropy'] = np.sum(sat_entropy[grid.core_nodes])
+
 ##### channel network
 #find number of saturated cells
 count_sat_nodes = np.sum(sat_all,axis=1)
@@ -374,7 +394,9 @@ output_fields = [
         'at_node:sat_mean_end_interstorm',
         'at_node:Q_mean_end_storm',
         'at_node:Q_mean_end_interstorm',
-
+        'at_node:saturation_class',
+        'at_node:saturation_probability',
+        'at_node:saturation_entropy',
         ]
 
 filename = '../post_proc/%s/grid_%d.nc'%(base_output_path, ID)
