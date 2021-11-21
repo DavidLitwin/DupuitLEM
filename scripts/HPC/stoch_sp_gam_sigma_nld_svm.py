a1c1ed49f8c8fad80b06f57e368cba35459ef7b2
David Litwin <davidglitwin@posteo.net>
2021-11-21 14:41:59 -0500

    start stoch with nld and svm

--- scripts/HPC/stoch_sp_gam_sigma_nld_svm.py
+++ scripts/HPC/stoch_sp_gam_sigma_nld_svm.py
@@ -17,7 +17,7 @@ import pickle
 from landlab import RasterModelGrid
 from landlab.components import (
     GroundwaterDupuitPercolator,
-    LinearDiffuser,
+    TaylorNonLinearDiffuser,
     FastscapeEroder,
     PrecipitationDistribution,
     )
@@ -57,8 +57,9 @@ def tb_fun(hg, p, n, gam, sigma, hi, rho):
     return (hg*n*gam)*(1-rho)/(p*sigma*hi)
 
 #generate dimensioned parameters
-def generate_parameters(p, n, v0, hg, lg, tg, gam, hi, sigma, rho, ai):
+def generate_parameters(p, n, v0, lg, tg, alpha, gam, hi, sigma, rho, ai):
 
+    hg = alpha*lg
     K = K_fun(v0, lg, tg)
     D = D_fun(lg, tg)
     U = U_fun(hg, tg)
@@ -69,19 +70,19 @@ def generate_parameters(p, n, v0, hg, lg, tg, gam, hi, sigma, rho, ai):
     tb = tb_fun(hg, p, n, gam, sigma, hi, rho)
     pet = ai*p
 
-    return K, D, U, ksat, p, pet, b, n, v0, hg, lg, tg, ds, tr, tb, gam, hi, sigma, rho, ai
+    return K, D, U, ksat, p, pet, b, n, v0, hg, lg, tg, ds, tr, tb, alpha, gam, hi, sigma, rho, ai
 
 
 #parameters
 sigma_all = np.array([8, 16, 32, 64, 128])
 gam_all = np.array([1.0, 2.0, 4.0, 8.0, 16.0])
-sigma1 = np.array(list(product(sigma_all, gam_all)))[:,0]
-gam1 = np.array(list(product(sigma_all, gam_all)))[:,1]
-ai = 1.5
+
+ai = 0.5
+sc = 0.5
 hi = 5.0
 rho = 0.03
+alpha = 0.15
 lg = 15 # geomorphic length scale [m]
-hg = 2.25 # geomorphic height scale [m]
 tg = 22500*(365*24*3600) # geomorphic timescale [s]
 v0 = 2.0*lg # contour width (also grid spacing) [m]
 n = 0.1 # drainable porosity [-]
@@ -94,15 +95,15 @@ dtg_max_nd = 2e-3 # maximum geomorphic timestep in units of tg [-]
 ksf_base = 500 # morphologic scaling factor
 Th_nd = 50 # hydrologic time in units of (tr+tb) [-]
 
-params = np.zeros((len(sigma1),20))
-for i in range(len(sigma1)):
-    params[i,:] = generate_parameters(p, n, v0, hg, lg, tg, gam1[i], hi, sigma1[i], rho, ai)
+params = []
+for sigma, gam in product(sigma_all, gam_all):
+    params.append(generate_parameters(p, n, v0, lg, tg, alpha, gam, hi, sigma, rho, ai))
 
-df_params = pandas.DataFrame(params,columns=['K', 'D', 'U', 'ksat', 'p', 'pet', 'b', 'n', 'v0', 'hg', 'lg', 'tg', 'ds', 'tr', 'tb', 'gam', 'hi', 'sigma', 'rho', 'ai'])
-df_params['alpha'] = df_params['hg']/df_params['lg']
+df_params = pandas.DataFrame(np.array(params),columns=['K', 'D', 'U', 'ksat', 'p', 'pet', 'b', 'n', 'v0', 'hg', 'lg', 'tg', 'ds', 'tr', 'tb', 'alpha', 'gam', 'hi', 'sigma', 'rho', 'ai'])
+df_params['Sc'] = sc
 df_params['td'] = (df_params['lg']*df_params['n'])/(df_params['ksat']*df_params['hg']/df_params['lg']) # characteristic aquifer drainage time [s]
 df_params['beta'] = (df_params['tr']+df_params['tb'])/df_params['td']
-df_params['hc'] = (df_params['p']*df_params['lg'])/(df_params['ksat']*df_params['hg']/df_params['lg']) # characteristic aquifer thickness [m]
+df_params['ha'] = (df_params['p']*df_params['lg'])/(df_params['ksat']*df_params['hg']/df_params['lg']) # characteristic aquifer thickness [m]
 df_params['Srange'] = Srange
 df_params['Tg'] = Tg_nd*df_params['tg'] # Total geomorphic simulation time [s]
 df_params['ksf'] = ksf_base/df_params['beta'] # morphologic scaling factor
@@ -110,7 +111,8 @@ df_params['Th'] = Th_nd*(df_params['tr']+df_params['tb']) # hydrologic simulatio
 df_params['dtg'] = df_params['ksf']*df_params['Th'] # geomorphic timestep [s]
 df_params['dtg_max'] = dtg_max_nd*df_params['tg'] # the maximum duration of a geomorphic substep [s]
 df_params['output_interval'] = (10/(df_params['dtg']/df_params['tg'])).round().astype(int)
-pickle.dump(df_params, open('parameters.p','wb'))
+if ID == 0:
+    pickle.dump(df_params, open('parameters.p','wb'))
 
 ksat = df_params['ksat'][ID]
 p = df_params['p'][ID]
@@ -128,6 +130,7 @@ D = df_params['D'][ID]
 U = df_params['U'][ID]
 hg = df_params['hg'][ID]
 lg = df_params['lg'][ID]
+Sc = df_params['Sc'][ID]
 
 Th = df_params['Th'][ID]
 Tg = df_params['Tg'][ID]
@@ -163,7 +166,8 @@ pdr = PrecipitationDistribution(grid, mean_storm_duration=tr,
     mean_interstorm_duration=tb, mean_storm_depth=ds,
     total_t=Th)
 pdr.seed_generator(seedval=1235)
-ld = LinearDiffuser(grid, linear_diffusivity=D)
+nld = TaylorNonLinearDiffuser(grid, linear_diffusivity=D, slope_crit=Sc, dynamic_dt=True)
+
 
 #initialize other models
 svm = SchenkVadoseModel(
@@ -186,7 +190,7 @@ rm = RegolithConstantThickness(grid, equilibrium_depth=b, uplift_rate=U)
 
 mdl = StreamPowerModel(grid,
         hydrology_model=hm,
-        diffusion_model=ld,
+        diffusion_model=nld,
         erosion_model=sp,
         regolith_model=rm,
         morphologic_scaling_factor=ksf,
