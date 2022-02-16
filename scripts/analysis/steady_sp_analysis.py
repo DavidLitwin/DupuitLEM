ec131cca7d389c809a8036b0dca1dc8479d285af
David Litwin <davidglitwin@posteo.net>
2022-02-16 15:36:16 -0500

    change output field

--- scripts/analysis/steady_sp_analysis.py
+++ scripts/analysis/steady_sp_analysis.py
@@ -250,7 +250,7 @@ shared_out = [
         'at_node:gw_flux_out_max',
         'at_node:gw_flux_in_max',
         'at_link:groundwater__specific_discharge',
-        'at_node:average_surface_water__specific_discharge',
+        'at_node:surface_water__discharge',
         ]
 
 output_fields = shared_out+raster_out if isinstance(mg, RasterModelGrid) else shared_out + hex_out
