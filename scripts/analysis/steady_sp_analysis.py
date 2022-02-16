dc99cb614f18012117f206f3ccd240ac28c29b94
David Litwin <davidglitwin@posteo.net>
2022-02-16 15:38:27 -0500

    change cour coefficient

--- scripts/analysis/steady_sp_analysis.py
+++ scripts/analysis/steady_sp_analysis.py
@@ -118,8 +118,8 @@ gdp = GroundwaterDupuitPercolator(mg,
           hydraulic_conductivity=Ks,
           regularization_f=0.01,
           recharge_rate=p,
-          courant_coefficient=0.01, #*Ks/1e-5,
-          vn_coefficient = 0.01, #*Ks/1e-5,
+          courant_coefficient=0.1, #*Ks/1e-5,
+          vn_coefficient = 0.1, #*Ks/1e-5,
 )
 
 hm = HydrologySteadyStreamPower(
