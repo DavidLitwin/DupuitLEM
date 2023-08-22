d91aeaacecfb78966fe047cc04ffc3c71385ba90
David Litwin <davidglitwin@posteo.net>
2023-08-22 14:36:12 +0200

    new scripts

--- scripts/orig_scripts/steady_sp_aniso_flat.py
+++ scripts/orig_scripts/steady_sp_aniso_flat.py
@@ -1,10 +1,10 @@
 """
-Steady recharge + constant baselevel + StreamPowerModel
+Steady recharge + constant thickness + StreamPowerModel
 
-Test of the constant baselevel, two-layer hydraulic conductivity 
-models. Otherwise using the dimensionless parameters from Litwin et al 2021
+This is a test of anisotropic hydraulic conductivity (in the x- and y-directions), otherwise
+using the characteristic scales and dimensionless parameters presented in Litwin et al. 2021. 
 
-Date: 22 August 2023
+Date: 14 August 2023
 """
 
 #%%
@@ -12,6 +12,7 @@ Date: 22 August 2023
 import os
 import numpy as np
 import pandas
+from numpy.linalg import inv
 
 from landlab import RasterModelGrid
 from landlab.components import (
@@ -24,7 +25,23 @@ from DupuitLEM.auxiliary_models import (
     HydrologySteadyStreamPower,
     RegolithConstantBaselevel,
     )
-from DupuitLEM.grid_functions import bind_avg_dual_ksat
+
+def get_link_hydraulic_conductivity(grid, K):
+    """Returns array of hydraulic conductivity on links, allowing for aquifers
+    with laterally anisotropic hydraulic conductivity.
+
+    Parameters
+    ----------
+    K: (2x2) array of floats (m/s)
+        The hydraulic conductivity tensor:
+        [[Kxx, Kxy],[Kyx,Kyy]]
+    """
+
+    u = grid.unit_vector_at_link
+    K_link = np.zeros(len(u))
+    for i in range(len(u)):
+        K_link[i] = np.dot(np.dot(u[i, :], K), u[i, :])
+    return K_link
 
 #%%
 #slurm info
@@ -61,15 +78,16 @@ def generate_parameters(p, n, v0, lg, tg, alpha, gam, hi):
     return K, D, U, ksat, p, b, n, v0, hg, lg, tg, alpha, gam, hi
 
 #parameters
-hi = 5.0 # aquifer relief index
-gam1 = np.array([1.0, 2.0, 4.0]) # drainage capacity index
-alpha = 0.5 # characteristic gradient
+hi = 5.0 # hillslope number
+gam = 4.0 # drainage capacity index
 lg = 15 # geomorphic length scale [m]
+alpha = 0.5 # characteristic gradient
 tg = 22500*(365*24*3600) # geomorphic timescale [s]
 v0 = 1.5*lg # contour width (also grid spacing) [m]
 n1 = 0.1 # drainable porosity [-]
 p1 = 0.75/(365*24*3600) # steady recharge rate
-k_ratio = 100
+rot = np.array([0, 30, 45, 60, 90])*np.pi/180 #rotation angle between grid axes and princpal axes of anisotropy
+k_ratio = 50 # ratio of minimum to maximum princpal hydraulic conductivities
 
 # time factors
 Tg_nd = 200 # total duration in units of tg [-]
@@ -77,31 +95,34 @@ dtg_nd = 2e-3 # geomorphic timestep in units of tg [-]
 Th = 30*24*3600 # hydrologic time per geomorphic step
 
 # keep all parameters fixed (just repeat them)
-params = np.zeros((len(gam1),14))
-for i in range(len(gam1)):
-    params[i,:] = generate_parameters(p1, n1, v0, lg, tg, alpha, gam1[i], hi)
+params = np.array(generate_parameters(p1, n1, v0, lg, tg, alpha, gam, hi)).reshape(1,14)
+params = np.repeat(params,len(rot), axis=0)
 df_params = pandas.DataFrame(params,columns=['K', 'D', 'U', 'ksat', 'p', 'b', 'n', 'v0', 'hg', 'lg', 'tg', 'alpha', 'gam', 'hi'])
+
+# add a non-condant rotation
+df_params['k_rot'] = rot
+df_params['k_ratio'] = k_ratio
+df_params['alpha'] = df_params['hg']/df_params['lg']
 df_params['td'] = (df_params['lg']*df_params['n'])/(df_params['ksat']*df_params['hg']/df_params['lg']) # characteristic aquifer drainage time [s]
 df_params['hc'] = (df_params['p']*df_params['lg'])/(df_params['ksat']*df_params['hg']/df_params['lg']) # characteristic aquifer thickness [m]
 df_params['Tg'] = Tg_nd*df_params['tg'] # Total geomorphic simulation time [s]
 df_params['dtg'] = dtg_nd*df_params['tg'] # geomorphic timestep [s]
 df_params['Th'] = Th
 df_params['ksf'] = df_params['dtg']/df_params['Th'] # morphologic scaling factor
-df_params['k_ratio'] = k_ratio
 
 #%% specific run
 
 df_params.loc[ID].to_csv('parameters.csv', index=True)
 
-#%%
 # pull values for this run
+kmax = df_params['ksat'][ID]
+kmin = kmax * 1/(df_params['k_ratio'][ID])
+krot = df_params['k_rot'][ID]
+
+
 p = df_params['p'][ID]
 b = df_params['b'][ID]
 n = df_params['n'][ID]
-
-ksat_upper = df_params['ksat'][ID]
-k_ratio = df_params['k_ratio'][ID]
-ksat_func = bind_avg_dual_ksat(ksat_upper/k_ratio, ksat_upper, b)
 
 K = df_params['K'][ID]
 Ksp = K/p # recharge rate from Q* goes in K
@@ -121,7 +142,7 @@ output["output_fields"] = [
         "at_node:aquifer_base__elevation",
         "at_node:water_table__elevation",
         ]
-output["base_output_path"] = './data/steady_two_layer_'
+output["base_output_path"] = './data/steady_aniso_'
 output["run_id"] = ID #make this task_id if multiple runs
 
 #initialize grid
@@ -139,10 +160,16 @@ base = grid.add_zeros('node', 'aquifer_base__elevation')
 wt = grid.add_zeros('node', 'water_table__elevation')
 wt[:] = elev.copy()
 
+# calculate link hydraulic conductivity
+rot = np.array([[np.cos(krot), -np.sin(krot)],[np.sin(krot), np.cos(krot)]])
+K_principal = np.array([[kmax,0.0],[0.0, kmin]])
+K = rot @ K_principal @ inv(rot)
+k1 = get_link_hydraulic_conductivity(grid, K)
+
 #initialize components
 gdp = GroundwaterDupuitPercolator(grid,
         porosity=n,
-        hydraulic_conductivity=ksat_func,
+        hydraulic_conductivity=k1,
         regularization_f=0.01,
         recharge_rate=p,
         courant_coefficient=0.1,
@@ -164,6 +191,7 @@ sp = FastscapeEroder(grid,
         discharge_field="surface_water_area_norm__discharge",
 )
 rm = RegolithConstantBaselevel(grid, uplift_rate=U)
+
 
 #%%
 mdl = StreamPowerModel(grid,
