1594a56000b1a1a68b2d7b3bc826becb75aa031c
David Litwin <davidglitwin@posteo.net>
2023-08-22 09:48:54 +0200

    Add two layer permeability func

--- DupuitLEM/grid_functions/grid_funcs.py
+++ DupuitLEM/grid_functions/grid_funcs.py
@@ -131,3 +131,68 @@ def bind_avg_recip_ksat(ks, d):
         return kavg
 
     return bound_avg_recip_ksat
+
+def bind_avg_dual_ksat(Ks_0, Ks_1, b_1):
+    """
+    Function to create the bound_avg_dual_ksat function. This function
+    is used when the hydraulic conductivity has a two-layer structure,
+    in which the lower layer is bounded at the bottom by the 
+    aquifer_base__elevation, and the upper layer bounded by a surface
+    a fixed distance b1 below topography z:
+
+             ( Ks_0,    zwt <= z - b1
+        k =  |
+             ( Ks_1,    zwt > z - b1
+
+    Parameters
+    ----------   
+    Ks_0: float
+        Hydraulic conductivity of lower layer.
+    ks_1: float
+        Hydraulic conductivity of upper layer.
+    b_1: float
+        Depth from surface to upper layer boundary.
+
+    Returns
+    -------
+    bound_avg_dual_ksat: function
+        Function to supply to GroundwaterDupuitPercolator
+    """
+
+    def bound_avg_dual_ksat(grid):
+        """
+        Calculate the average hydraulic conductivity for a two-layer 
+        subsurface structure:
+
+             ( Ks_0,    zwt <= z - b1
+        k =  |
+             ( Ks_1,    zwt > z - b1
+
+        Parameters
+        ----------
+        grid: ModelGrid
+            Landlab ModelGrid object. 
+
+        Returns
+        -------
+        kavg: array
+            Array of effective hydrualic conductivities given water table position
+            and a depth-dependent hydraulic conductivity model.
+        """
+
+        # grid fields
+        z = grid.at_node["topographic__elevation"]
+        z0 = grid.at_node["aquifer_base__elevation"]
+        z1 = z - b_1
+        zwt = grid.at_node["water_table__elevation"]
+
+        # calc avg if wt in upper layer, then conditional set if in lower layer
+        Ks_node = ((z1-z0)*Ks_0 + (zwt - z1)*Ks_1)/(zwt-z0)
+        Ks_node[zwt<=z1] = Ks_0
+
+        # map to links
+        Kavg = map_mean_of_link_nodes_to_link(grid, Ks_node)
+
+        return Kavg
+
+    return bound_avg_dual_ksat
