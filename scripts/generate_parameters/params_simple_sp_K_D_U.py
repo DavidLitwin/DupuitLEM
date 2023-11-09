3d55a5315fdc0f7888ce3e53966444042b5090d4
David Litwin <davidglitwin@posteo.net>
2023-11-09 14:02:00 +0100

    new simple lem

---
+++ scripts/generate_parameters/params_simple_sp_K_D_U.py
@@ -0,0 +1,44 @@
+"""
+Generate parameters for simple streampower model, varying K, D, U, and v0.
+
+New Nov. 2023.
+
+"""
+
+import os
+import numpy as np
+import pandas as pd
+from itertools import product
+
+task_id = os.environ['SLURM_ARRAY_TASK_ID']
+ID = int(task_id)
+
+
+D_all = [0.005, 0.0005]
+U_all = [1e-4, 1e-5]
+K_all = np.linspace(1e-4, 1e-6, 10)
+v0_all = [5, 10, 50]
+Nx = 200
+Ny = 400
+T = 5e7
+dt = 500
+m = 0.5
+n = 1
+Sc = 0.0
+routing_method = 'D8'
+save_transients = True
+r_condition = 1e-8
+
+prod = np.array(list(product(K_all, D_all, U_all, v0_all)))
+df_params = pd.DataFrame(prod, columns=['K', 'D', 'U', 'v0'])
+df_params['Nx'] = Nx
+df_params['Ny'] = Ny
+df_params['T'] = T
+df_params['dt'] = dt
+df_params['m'] = m
+df_params['n'] = n
+df_params['Sc'] = Sc
+df_params['routing_method'] = routing_method
+df_params['save_transients'] = save_transients
+
+df_params.loc[ID].to_csv('parameters.csv', index=True)
---
+++ scripts/run_models/model_simple_sp.py
@@ -0,0 +1,140 @@
+
+import os
+import numpy as np
+from tqdm import tqdm
+import pandas
+
+from landlab import RasterModelGrid
+from landlab.components import (
+    FlowAccumulator,
+    FastscapeEroder,
+    LinearDiffuser,
+    TaylorNonLinearDiffuser,
+    FlowDirectorD8,
+    FlowDirectorSteepest,
+    LakeMapperBarnes,
+    DepressionFinderAndRouter,
+    ChiFinder,
+    )
+from landlab.io.netcdf import to_netcdf
+
+import matplotlib.pyplot as plt
+
+#slurm info
+task_id = os.environ['SLURM_ARRAY_TASK_ID']
+ID = int(task_id)
+
+try:
+    df_params = pandas.read_csv('parameters.csv', index_col=0)[task_id]
+    # df = pd.read_csv('df_params_1d_%d.csv'%ID, index_col=0)
+except FileNotFoundError:
+    print("Supply a parameter file, 'parameters.csv' with column title equal to TASK_ID")
+
+Nx = df_params['Nx']
+Ny = df_params['Ny']
+T = df_params['T']
+dt = df_params['dt']
+v0 = df_params['v0']
+D = df_params['D']
+K = df_params['K']
+U = df_params['U']
+m = df_params['m']
+n = df_params['n']
+Sc = df_params['Sc']
+routing_method = df_params['routing_method']
+save_transients = df_params['save_transients']
+r_condition = df_params['r_condition']
+output_interval = df_params['output_interval']
+save_directory = './data/simple_sp_'
+
+N = int(T//dt)
+output_interval = 500
+
+np.random.seed(12345)
+grid = RasterModelGrid((Nx, Ny), xy_spacing=v0)
+grid.set_status_at_node_on_edges(right=grid.BC_NODE_IS_CLOSED,
+                                top=grid.BC_NODE_IS_FIXED_VALUE,
+                                left=grid.BC_NODE_IS_CLOSED,
+                                bottom=grid.BC_NODE_IS_FIXED_VALUE,
+)
+z = grid.add_zeros('node', 'topographic__elevation')
+z[:] = 0.5*np.random.rand(len(z))
+
+# depression_finder='DepressionFinderAndRouter', routing='D8'
+# fa = FlowAccumulator(grid, surface='topographic__elevation', flow_director='D8', depression_finder='LakeMapperBarnes', method='D8')
+if routing_method == "D8":
+    fd = FlowDirectorD8(grid)
+elif routing_method == "Steepest":
+    fd = FlowDirectorSteepest(grid)
+else:
+    raise ValueError("routing_method must be either D8 or Steepest.")
+fa = FlowAccumulator(
+    grid,
+    surface="topographic__elevation",
+    flow_director=fd,
+)
+lmb = LakeMapperBarnes(
+    grid,
+    method=routing_method,
+    fill_flat=False,
+    surface="topographic__elevation",
+    fill_surface="topographic__elevation",
+    redirect_flow_steepest_descent=False,
+    reaccumulate_flow=False,
+    track_lakes=False,
+    ignore_overfill=True,
+)
+dfr = DepressionFinderAndRouter(grid)
+if Sc > 0:
+    ld = TaylorNonLinearDiffuser(grid, linear_diffusivity=D, slope_crit=Sc, dynamic_dt=True)
+else:
+    ld = LinearDiffuser(grid, linear_diffusivity=D)
+sp = FastscapeEroder(grid, K_sp=K, m_sp=m, n_sp=n)
+
+
+relief = np.zeros(N)
+
+for i in tqdm(range(N), desc="Completion"):
+
+    # baselevel change
+    z[grid.core_nodes] += U*dt
+
+    # diffusion
+    ld.run_one_step(dt)
+
+    # flow directions and incision
+    dfr._find_pits()
+    if dfr._number_of_pits > 0:
+        lmb.run_one_step()
+    fa.run_one_step()
+    sp.run_one_step(dt)
+
+    # save output
+    if save_transients:
+        if i%output_interval==0:
+            filename = os.path.join(save_directory, '%d_grid_%d.nc'%(ID,i))
+            to_netcdf(grid, filename)
+
+    # check relief condition
+    relief[i] = np.mean(z[grid.core_nodes])
+    if i > 100:
+        if np.mean(np.abs(np.diff(relief[i-50:i])/dt)) < r_condition:
+            break
+
+# calculate chi everywhere
+cf = ChiFinder(grid, min_drainage_area=100*v0**2, reference_concavity=m/n, reference_area=v0**2)
+cf.calculate_chi()
+
+# save things
+
+# save grid
+filename = os.path.join(save_directory, 'grid_%d.nc'%ID)
+to_netcdf(grid, filename)
+
+# topography
+plt.figure()
+grid.imshow("topographic__elevation", colorbar_label='Elevation [m]')
+plt.savefig(os.path.join(save_directory, 'elevation_%d.png'%ID))
+
+# save relief
+np.savetxt(os.path.join(save_directory, 'relief_%d.csv'%ID), relief, delimiter=',')
