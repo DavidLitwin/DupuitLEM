c3ea91005adf1eec07610c638dd1bae5a402a140
DavidLitwin <davidglitwin@posteo.net>
2026-02-06 16:25:12 -0500

    new params

--- scripts/generate_parameters/params_stoch_nld_svm_ai_gam_sigma.py
+++ scripts/generate_parameters/params_stoch_nld_svm_ai_gam_sigma.py
@@ -69,12 +69,10 @@ def generate_parameters(p, ne, v0, hg, lg, tg, gam, beta, sigma, rho, ai, theta,
     tr = tr_fun(hg, p, ne, gam, sigma, beta, rho)
     tb = tb_fun(hg, p, ne, gam, sigma, beta, rho)
 
-
-
     return K, D, U, ksat, p, pet, b, ne, na, v0, hg, lg, tg, E0, ds, tr, tb, alpha, gam, beta, sigma, rho, ai, theta, phi
 
 
-# params for both hyd1d recharge estimation and lem
+# params
 sigma_all = np.geomspace(8.0, 128.0, 3)
 gam_all = np.geomspace(1.0, 16.0, 3)
 ai_all = [0.2, 0.4, 0.8]
@@ -87,13 +85,13 @@ rho = 0.1
 phi = 1.5
 lg = 15 # geomorphic length scale [m]
 hg = alpha * lg
-tg = 22500*(365*24*3600) # geomorphic timescale [s]
+tg = 10000*(365*24*3600) # geomorphic timescale [s]
 v0 = 2.0*lg # contour width (also grid spacing) [m]
 ne = 0.1 # drainable porosity [-]
 p = 1.0/(365*24*3600) # average precip rate
 
 Tg_nd = 2000 # total duration in units of tg [-]
-dtg_max_nd = 2e-3 # maximum geomorphic timestep in units of tg [-]
+dtg_max_nd = 5e-2 # maximum geomorphic timestep in units of tg [-]
 ksf_base = 500 # morphologic scaling factor
 Th_nd = 25 # hydrologic time in units of (tr+tb) [-]
 
@@ -111,15 +109,34 @@ df_params['Nx'] = Nx
 df_params['td'] = (df_params['lg']*df_params['ne'])/(df_params['ksat']*df_params['hg']/df_params['lg']) # characteristic aquifer drainage time [s]
 df_params['ha'] = (df_params['p']*df_params['lg'])/(df_params['ksat']*df_params['hg']/df_params['lg']) # characteristic aquifer thickness [m]
 df_params['Tg'] = Tg_nd*df_params['tg'] # Total geomorphic simulation time [s]
-df_params['ksf'] = ksf_base/df_params['beta'] # morphologic scaling factor
+# df_params['ksf'] = ksf_base/df_params['beta'] # morphologic scaling factor
 df_params['Th'] = Th_nd*(df_params['tr']+df_params['tb']) # hydrologic simulation time [s]
-df_params['dtg'] = df_params['ksf']*df_params['Th'] # geomorphic timestep [s]
-df_params['dtg_max'] = dtg_max_nd*df_params['tg'] # the maximum duration of a geomorphic substep [s]
-df_params['output_interval'] = (10/(df_params['dtg']/df_params['tg'])).round().astype(int)
+df_params['dtg'] = dtg_max_nd*df_params['tg']#df_params['ksf']*df_params['Th'] # geomorphic timestep [s]
+df_params['dtg_max'] = 200*365*24*3600 #dtg_max_nd*df_params['tg'] # the maximum duration of a geomorphic substep [s]
+df_params['ksf'] = df_params['dtg']/df_params['Th'] # morphologic scaling factor
+df_params['output_interval'] = (20/(df_params['dtg']/df_params['tg'])).round().astype(int)
 
-#%%
+# %% save parameters for this SLURM array task
 
+# use SLURM_ARRAY_TASK_ID to select parameter set and save to CSV
 task_id = os.environ['SLURM_ARRAY_TASK_ID']
 ID = int(task_id)
 
 df_params.loc[ID].to_csv('parameters.csv', index=True)
+
+#%% # Convert time units from seconds to years and rates from per second to per year for easier interpretability
+
+def convert_sec_to_yr(seconds):
+    return seconds / (365*24*3600)
+
+def convert_inverse_sec_to_per_yr(inverse_seconds):
+    return inverse_seconds * (365*24*3600)
+
+cols_with_sec = ['tg', 'tr', 'tb', 'td', 'Tg', 'Th', 'dtg', 'dtg_max']
+cols_inverse_sec = ['p', 'pet', 'ksat', 'K', 'D', 'U', 'E0']
+
+df_params_yr = df_params.copy()
+df_params_yr[cols_with_sec] = df_params_yr[cols_with_sec].applymap(convert_sec_to_yr)
+df_params_yr[cols_inverse_sec] = df_params_yr[cols_inverse_sec].applymap(convert_inverse_sec_to_per_yr)
+
+# %%
